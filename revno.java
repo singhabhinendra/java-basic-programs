24e03f432295595766d55f4f77cc7b63bbb3678f
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-20 08:05:20 +0530

    Add files via upload

---
+++ revno.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class revno {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int i=101;
+            while(i>0){
+                i--;
+                System.out.println(i);
+        }
+    }
+}
