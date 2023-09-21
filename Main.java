a438c3a89437dc098544956c7a04c7ce84f262ad
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-21 11:14:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
-//write a java program to print factorial of a number..
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int i,  ;
-        for(i=0;i<=n:i++){
-            
-        }
-
-
-        
     }
 }
