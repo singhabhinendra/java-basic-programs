7b162fdb117d2f009dcbc1a4244d6874c5d4cfda
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-22 21:53:53 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("enter the string");
+        Scanner sc=new Scanner(System.in);
+        char p=new char();
+        
+    }
+}
