a8ace27af5691ae0ec9882779d9950bbb98bf5cd
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-23 22:52:10 +0530

    Add files via upload

---
+++ maxno.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the no s;");
+    int s=sc.nextInt();
+    System.out.println("enter the no t");
+    int t=sc.nextInt();
+    if (s>t){
+        System.out.println("S is maximum no");
+    }
+    else {
+        System.out.println(" t is maximum no");
+    }
+
+    }
+}
