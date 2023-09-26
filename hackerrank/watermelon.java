7a38d5f2d00377c8d14366889b5304c277faedbe
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-27 01:36:33 +0530

    Create watermelon.java

---
+++ hackerrank/watermelon.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner sc=new Scanner(System.in);
+    int w=sc.nextInt();
+    
+    if(w%2==0&&w>2){
+        
+        System.out.println("YES");
+    }
+    else{
+        System.out.println("NO");
+    }
+        
+    }
+}
