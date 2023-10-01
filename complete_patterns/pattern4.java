157166012acd5996ada8e2d4df27fa36b97ddfe3
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-10-01 20:43:03 +0530

    Add files via upload

---
+++ complete_patterns/pattern4.java
@@ -0,0 +1,13 @@
+public class pattern4 {
+    public static void main(String[] args) {
+        pattern(10);
+    }
+    static void pattern(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" " );
+            }
+            System.out.println( );
+        }
+    }
+}
