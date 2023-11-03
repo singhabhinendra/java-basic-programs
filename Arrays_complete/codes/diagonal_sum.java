b19a0b124201584da37162ef4b3562189c7357af
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-11-04 03:19:06 +0530

    Create diagonal_sum.java

---
+++ Arrays_complete/codes/diagonal_sum.java
@@ -0,0 +1,26 @@
+//it is not optimize code because of time complexity O(n^2);
+//it is bruteforce code;
+
+class diagonal {
+    public static int diagonalsum(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        System.out.println( diagonalsum(matrix));
+
+    }
+
+    }
