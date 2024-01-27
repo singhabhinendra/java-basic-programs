525a3b8b6dde7911f62d3800750f4b6f31808f4a
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2024-01-27 17:33:04 +0530

    Create Implement_lower_bound.java

---
+++ Arrays_complete/codes/Implement_lower_bound.java
@@ -0,0 +1,22 @@
+//by using  Binary search Algorithm ,we have find the lowerbound in the given array;
+// Time Complexity is O(log n)
+public class Solution {
+    public static int lowerBound(int []arr, int n, int x) {
+        // Write your code here
+        int low=0,high=n-1;
+        int ans=n;
+        while (low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+
+            }
+        }
+        return ans;
+
+    }
+}
